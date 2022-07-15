584bc896aaa130234069993ff21ec7e53331d9ab
Kirlbert <kirlbert@gmail.com>
2022-07-14 20:03:29 -0400

    add user logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser midware to the API router
     // if curr user session is valid, req.user = curr user
     // if curr user is invalid, req.user is null
 router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
