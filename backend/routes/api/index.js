1578c60dba12a448468063c50dda8a36fc4f5b2e
Kirlbert <kirlbert@gmail.com>
2022-08-27 18:25:28 -0400

    front end csrf protection set up

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,9 +11,9 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
+// router.post('/test', (req, res) => {
+//     res.json({ requestBody: req.body });
+// });
 
 
 module.exports = router;
