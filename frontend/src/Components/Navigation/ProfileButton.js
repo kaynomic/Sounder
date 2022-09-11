fdebf5e53458177e5276c99efd6848654381979c
Kirlbert <kirlbert@gmail.com>
2022-09-11 16:05:44 -0400

    added css to logout button

--- frontend/src/Components/Navigation/ProfileButton.js
+++ frontend/src/Components/Navigation/ProfileButton.js
@@ -44,7 +44,7 @@ function ProfileButton({ user }) {
             <li>{user.username}</li>
             <li>{user.email}</li>
             <li>
-              <button onClick={logout}>Log Out</button>
+              <button onClick={logout} className="log-out-button">Log Out</button>
             </li>
           </ul>
         )}
